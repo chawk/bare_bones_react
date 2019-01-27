ceee5f7f052da3c117b76f4016e1dc8a40293614
chris <chrshawkes@yahoo.com>
2019-01-27 17:41:53 -0500

    updated express js web server and readme

--- server/index.js
+++ server/index.js
@@ -4,4 +4,4 @@ const port = 9081
 
 app.get('/', (req, res) => res.sendFile(__dirname + "/index.html"))
 app.use('/static', express.static('../dist'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Bare Bones React - App is listening on port ${port}!`))
